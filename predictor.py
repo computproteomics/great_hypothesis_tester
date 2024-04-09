7d5bb390046535bfd78588c3bd9631c9fd266fe2
DavidDeutz <deutz.david@gmx.at>
2024-04-09 10:01:30 +0200

    added alpha pept model to predictor

--- predictor.py
+++ predictor.py
@@ -3,83 +3,214 @@ import csv
 import pandas as pd
 import numpy as np
 
+import json
+
 from koinapy import Koina
 
 """
 Global constant values
 """
-KOINA_MODEL = 'Prosit_2019_irt'
-KOINA_SERVER = 'koina.wilhelmlab.org:443'
-DEEPLC_QUERY = 'deeplc.csv'
-DEEPLC_RESULT = DEEPLC_QUERY.split(sep='.')[0] + '_deeplc_predictions.csv'
+KOINA_PROSIT_2019_RT_MODEL = 'Prosit_2019_irt'
+KOINA_DEEPLC_RT_MODEL = "Deeplc_hela_hf"
+KOINA_ALPHAPEPT_RT_MODEL = "AlphaPept_rt_generic"
 
+KOINA_PROSIT_2019_FRAG_MODEL = 'Prosit_2019_intensity'
+KOINA_DEEPLC_FRAG_MODEL = 'ms2pip_2021_HCD'
+KOINA_ALPHAPEPT_FRAG_MODEL = 'AlphaPept_ms2_generic'
+
+KOINA_SERVER = 'koina.wilhelmlab.org:443'
+
+PRECURSOR_CHARGE = 1
+COLLISION_ENERGY = 25
+INSTRUMENT_TYPE = 'QE'
+
+MAX_SEQ_LENGTH = 30
 
 class Predictor:
 
 
-    def predict_retention_time(self, input_file: str, result_file: str) -> None:
-
-        """
-        Predicts the retention time of peptide sequences using the deep learning models of 'Prosit' and 'DeepLC'.
-
-        Parameters
-        ----------
-        input_file: str
-            The path to the input .csv file which contains the sequences.
-
-        result_file: str
-            The path to the output .csv file where the predictions should be written to.
-
-        Raises
-        ------
-        AttributeError
-            If the given input file does not exist.
-        """
+    def predict_retention_time(self, input_file: str, sequence_type: str = 'random') -> None:
 
         if not os.path.exists(input_file):
             raise AttributeError(f'File {input_file} does not exist!')
-        
-        # read csv file
-        rt_input = pd.read_csv(input_file)
 
         # ----------------------------------------------------------------- #
-        # use koina api for prediction                                      #
+        # prepare retention time input                                      #
         # ----------------------------------------------------------------- #
 
-        # map inputs
-        koina_input = pd.DataFrame()
-        koina_input['peptide_sequences'] = rt_input['seq'].values
+        # read .csv file
+        input = pd.read_csv(input_file)
+
+        rt_input = pd.DataFrame()
+        rt_input['peptide_sequences'] = input['seq'].values
+
+        # ----------------------------------------------------------------- #
+        # use 2019 Prosit model for prediction                              #
+        # ----------------------------------------------------------------- #
 
         # build predictor
-        koina_predictor = Koina(KOINA_MODEL, KOINA_SERVER)
+        prosit_2019_predictor = Koina(KOINA_PROSIT_2019_RT_MODEL, KOINA_SERVER)
 
         # make predictions
-        koina_predictions = koina_predictor.predict(koina_input)['irt'].flatten()
+        prosit_2019_prediction = prosit_2019_predictor.predict(rt_input)['irt'].flatten()
 
         # ----------------------------------------------------------------- #
-        # use deeplc for prediction                                         #
+        # use DeepLC model for prediction                                   #
         # ----------------------------------------------------------------- #
 
-        deeplc_field = ['seq', 'modifications']
-        deeplc_rows = [[i, ''] for i in rt_input['seq'].values]
+        # build predictor
+        deeplc_predictor = Koina(KOINA_DEEPLC_RT_MODEL, KOINA_SERVER)
 
-        # write to temporary csv file
-        with open(DEEPLC_QUERY, 'w') as tmp:
-            csvwriter = csv.writer(tmp)
-            csvwriter.writerow(deeplc_field)
-            csvwriter.writerows(deeplc_rows)
+        # make predictions
+        deeplc_prediction = deeplc_predictor.predict(rt_input)['irt'].flatten()
 
-        # load predictions
-        os.system(f'deeplc --file_pred {DEEPLC_QUERY}')
+        # ----------------------------------------------------------------- #
+        # use AlphaPept model for prediction                                #
+        # ----------------------------------------------------------------- #
+        
+        # build predictor
+        alphapept_predictor = Koina(KOINA_ALPHAPEPT_RT_MODEL, KOINA_SERVER)
 
-        # parse results
-        deeplc_prediction = np.array(pd.read_csv(DEEPLC_RESULT).iloc[:, 1].values)
+        # make predictions
+        alphapept_prediction = alphapept_predictor.predict(rt_input)['irt'].flatten()
 
-        os.remove(DEEPLC_QUERY)
-        os.remove(DEEPLC_RESULT)
+        # ----------------------------------------------------------------- #
+        # write results to file                                             #
+        # ----------------------------------------------------------------- #
 
-        rt_input['Prosit Prediction'] = koina_predictions
-        rt_input['DeepLC Prediction'] = deeplc_prediction
+        # build result dataframe
+        rt_output = pd.DataFrame()
+        rt_output['Sequence'] = rt_input['peptide_sequences']
+        rt_output['Sequence Length'] = rt_input['peptide_sequences'].str.len().values
+        rt_output['Sequence Type'] = np.array([sequence_type]* len(input['seq'].values))
+        rt_output['Prosit Prediction'] = prosit_2019_prediction
+        rt_output['DeepLC Prediction'] = deeplc_prediction
+        rt_output['Alpha Pept Prediction'] = alphapept_prediction
 
-        rt_input.to_csv(result_file)
+        # write result to .csv file
+        result_filename = '{}_irt_results.csv'.format(input_file.split('.')[0])
 
+        rt_output.to_csv(result_filename, index=False)
+
+
+    def predict_fragmentation(self, input_file: str, sequence_type: str = 'random') -> None:
+        
+        if not os.path.exists(input_file):
+            raise AttributeError(f'File {input_file} does not exist!')
+
+        # ----------------------------------------------------------------- #
+        # prepare fragmentation input                                       #
+        # ----------------------------------------------------------------- #
+        
+        # read .csv file
+        input = pd.read_csv(input_file)
+
+        frag_input = pd.DataFrame()
+        frag_input['peptide_sequences'] = input['seq'].values
+        frag_input['precursor_charges'] = np.array([PRECURSOR_CHARGE] * len(input['seq'].values))
+        frag_input['collision_energies'] = np.array([COLLISION_ENERGY] * len(input['seq'].values))
+        frag_input['instrument_types'] = np.array([INSTRUMENT_TYPE] * len(input['seq'].values))
+
+        # ----------------------------------------------------------------- #
+        # use Prosit model for prediction                                   #
+        # ----------------------------------------------------------------- #
+
+        # build predictor
+        prosit_2019_predictor = Koina(KOINA_PROSIT_2019_FRAG_MODEL, KOINA_SERVER)
+
+        # make predictions
+        prosit_2019_prediction = prosit_2019_predictor.predict(frag_input)
+
+        # ----------------------------------------------------------------- #
+        # use DeepLC model for prediction                                   #
+        # ----------------------------------------------------------------- #
+
+        # build predictor
+        deeplc_predictor = Koina(KOINA_DEEPLC_FRAG_MODEL, KOINA_SERVER)
+
+        # make predictions
+        deeplc_prediction = deeplc_predictor.predict(frag_input)
+
+        # ----------------------------------------------------------------- #
+        # use AlphaPept model for prediction                                #
+        # ----------------------------------------------------------------- #
+
+        # build predictor
+        alphapept_predictor = Koina(KOINA_ALPHAPEPT_FRAG_MODEL, KOINA_SERVER)
+
+        alphapept_prediction = alphapept_predictor.predict(frag_input)
+
+        # ----------------------------------------------------------------- #
+        # map API responses                                                 #
+        # ----------------------------------------------------------------- #
+
+        prosit_2019_result_df = self.__map_koina_prediction__(prosit_2019_prediction)
+        deeplc_result_df = self.__map_koina_prediction__(deeplc_prediction, 'deeplc')
+        alphapept_result_df = self.__map_koina_prediction__(alphapept_prediction, 'alphapept')
+
+        # ----------------------------------------------------------------- #
+        # write results to file                                             #
+        # ----------------------------------------------------------------- #
+
+        frag_output = pd.DataFrame()
+        frag_output['Sequence'] = frag_input['peptide_sequences']
+        frag_output['Sequence Length'] = frag_input['peptide_sequences'].str.len().values
+        frag_output['Sequence Type'] = np.array([sequence_type]* len(input['seq'].values))
+
+        frag_output = pd.concat([frag_output,prosit_2019_result_df, deeplc_result_df, alphapept_result_df], axis=1, ignore_index=False)
+
+        # write result to .csv file
+        result_filename = '{}_fragmentation_results.csv'.format(input_file.split('.')[0])
+
+        frag_output.to_csv(result_filename, index=False)
+
+
+    def __map_koina_prediction__(self, prediction: dict, predictor: str = 'prosit') -> pd.DataFrame:
+        
+        if prediction is None or len(prediction) == 0:
+            raise ValueError('No predictions found!')
+        
+        frames = []
+
+        # build dataframe for each sequence
+        for i in range(len(prediction['intensities'])):
+
+            # extract information
+            current_intensities = prediction['intensities'][i]
+            current_mz = prediction['mz'][i]
+            current_annotations = prediction['annotation'][i]
+
+            # build df
+            current_data = pd.DataFrame(data={'seq': [i] * len(current_intensities),
+                                              'intensities': current_intensities,
+                                              'mz': current_mz,
+                                              'annotation': current_annotations})
+                
+            # filter ions with single charge
+            current_data = current_data[current_data['annotation'].apply(lambda x: x.endswith(b'+1'))]
+
+            # pivot df
+            current_data = current_data.pivot(index='seq', 
+                                              columns='annotation',
+                                              values=['intensities', 'mz'])
+
+            # append to temporary frames
+            frames.append(current_data)
+
+        # construct final df
+        result_frame = pd.concat(frames, axis=0, ignore_index=True)
+
+        # remove ions that were not found
+        not_found_ions = result_frame.columns[result_frame.eq(-1).all()]
+        result_frame.drop(columns=not_found_ions, inplace=True)
+
+        # rename columns
+        rename_column = lambda column: f"{column[1].decode('utf-8').split('+')[0]}_{'intens' if column[0] == 'intensities' else 'mz'}_{predictor}"
+
+        result_frame.columns = [rename_column(column) for column in result_frame.columns]
+
+        return result_frame
+
+
+predictor = Predictor()
+predictor.predict_fragmentation('2k_7length_aromatic.csv')
